9d66b2eafc73325d8aee5de50a8854e7971d9dfb
ferchusmh <66027731+ferchusmh@users.noreply.github.com>
2020-07-11 14:57:45 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace maquina_dispensadora_de_bebidas_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Image = global::maquina_dispensadora_de_bebidas_1.Properties.Resources.GIF3;
+            this.button1.Location = new System.Drawing.Point(31, 577);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(299, 57);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::maquina_dispensadora_de_bebidas_1.Properties.Resources.coca_cola_in_the_rainbow_by_princesssaphron_d389szv;
+            this.pictureBox1.Location = new System.Drawing.Point(-3, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(801, 808);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 711);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form2.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace maquina_dispensadora_de_bebidas_1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Lucida Calligraphy", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(198, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(401, 33);
+            this.label1.TabIndex = 33;
+            this.label1.Text = "SELECCIONA TU FAVORITA";
+            // 
+            // button15
+            // 
+            this.button15.Image = global::maquina_dispensadora_de_bebidas_1.Properties.Resources.Imagen15;
+            this.button15.Location = new System.Drawing.Point(639, 505);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(130, 136);
+            this.button15.TabIndex = 32;
+            this.button15.Text = "button15";
+            this.button15.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Image = global::maquina_dispensadora_de_bebidas_1.Properties.Resources.Imagen14;
+            this.button14.Location = new System.Drawing.Point(483, 504);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(133, 137);
+            this.button14.TabIndex = 31;
+            this.button14.Text = "button14";
+            this.button14.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Image = global::maquina_dispensadora_de_bebidas_1.Properties.Resources.Imagen13;
+            this.button13.Location = new System.Drawing.Point(327, 504);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(131, 137);
+            this.button13.TabIndex = 30;
+            this.button13.Text = "button13";
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Image = global::maquina_dispensadora_de_bebidas_1.Properties.Resources.Imagen12;
+            this.button12.Location = new System.Drawing.Point(183, 505);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(129, 136);
+            this.button12.TabIndex = 29;
+            this.button12.Text = "button12";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Image = global::maquina_dispensadora_de_bebidas_1.Properties.Resources.Imagen11;
+            this.button11.Location = new System.Drawing.Point(22, 504);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(137, 137);
+            this.button11.TabIndex = 28;
+            this.button11.Text = "button11";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Image = global::maquina_dispensadora_de_bebidas_1.Properties.Resources.Imagen10;
+            this.button10.Location = new System.Drawing.Point(639, 305);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(130, 135);
+            this.button10.TabIndex = 27;
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Image = global::maquina_dispensadora_de_bebidas_1.Properties.Resources.Imagen9;
+            this.button9.Location = new System.Drawing.Point(483, 307);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(133, 133);
+            this.button9.TabIndex = 26;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Image = global::maquina_dispensadora_de_bebidas_1.Properties.Resources.Imagen8;
+            this.button8.Location = new System.Drawing.Point(327, 305);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(131, 135);
+            this.button8.TabIndex = 25;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Image = global::maquina_dispensadora_de_bebidas_1.Properties.Resources.Imagen7;
+            this.button7.Location = new System.Drawing.Point(183, 305);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(129, 135);
+            this.button7.TabIndex = 24;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Image = global::maquina_dispensadora_de_bebidas_1.Properties.Resources.Imagen6;
+            this.button6.Location = new System.Drawing.Point(22, 305);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(137, 135);
+            this.button6.TabIndex = 23;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Image = global::maquina_dispensadora_de_bebidas_1.Properties.Resources.Imagen5;
+            this.button5.Location = new System.Drawing.Point(639, 96);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(130, 135);
+            this.button5.TabIndex = 22;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Image = global::maquina_dispensadora_de_bebidas_1.Properties.Resources.Imagen4;
+            this.button4.Location = new System.Drawing.Point(483, 96);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(133, 135);
+            this.button4.TabIndex = 21;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Image = global::maquina_dispensadora_de_bebidas_1.Properties.Resources.Imagen3;
+            this.button3.Location = new System.Drawing.Point(327, 96);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 135);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Image = global::maquina_dispensadora_de_bebidas_1.Properties.Resources.Imagen2;
+            this.button2.Location = new System.Drawing.Point(183, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 135);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::maquina_dispensadora_de_bebidas_1.Properties.Resources.Imagen1;
+            this.button1.Location = new System.Drawing.Point(22, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 135);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::maquina_dispensadora_de_bebidas_1.Properties.Resources._73150984_fondo_de_cubos_de_hielo_pila_de_cubos_de_hielo_azul_representación_3d;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 698);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 711);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
